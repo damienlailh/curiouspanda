28e0ffdf6d5c88b9ce17732f806a94d5d02f79ab
damienlailh <damienlailh@gmail.com>
2026-05-02 15:59:09 +0800

    Update loadNav.js

--- loadNav.js
+++ loadNav.js
@@ -1,17 +1,7 @@
-const basePath = "/curiouspanda";
-
-fetch(`${basePath}/components/nav.html`)
+fetch("/components/nav.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("navbar").innerHTML = data;
-
-    document.querySelectorAll("[data-link]").forEach(link => {
-      link.href = basePath + link.getAttribute("data-link");
-    });
-
-    document.querySelectorAll("[data-src]").forEach(img => {
-      img.src = basePath + img.getAttribute("data-src");
-    });
   })
   .catch(error => {
     console.error("Error loading navbar:", error);
